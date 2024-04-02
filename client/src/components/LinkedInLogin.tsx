1f77415ef25a8cc7c2800c0c6828894f7751dc5d
ngobiea <ngobiea@gmail.com>
2024-04-02 22:31:22 +0500

    oauth app

--- client/src/components/LinkedInLogin.tsx
+++ client/src/components/LinkedInLogin.tsx
@@ -4,7 +4,7 @@ const LinkedInLogin = () => {
   return (
     <button
       onClick={() => {
-        window.location.href = 'http://localhost:8081/authLinkedIn';
+        window.location.href = 'http://52.66.245.3:8081/authLinkedIn';
       }}
       className='w-full flex text-white justify-center  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
     >
